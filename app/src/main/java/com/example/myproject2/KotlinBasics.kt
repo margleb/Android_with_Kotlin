00c7e2fc7dc0b66e77744081c024de6fd69913be
margleb <margleb@yandex.ru>
2020-04-25 14:48:00 +0300

    Variables and difference between val and var

---
+++ app/src/main/java/com/example/myproject2/KotlinBasics.kt
@@ -0,0 +1,10 @@
+package com.example.myproject2
+
+fun main() {
+    // изменяемая переменная
+    var myName = "Gleb"
+    myName = "Franzis"
+    // Не изменяемая переменная
+    val mySurname = "Marianov";
+    print("Hello " + myName);
+}
