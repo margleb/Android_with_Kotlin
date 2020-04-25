29eeed17fa5416dcb085d66b7f0f4e53fd921b19
margleb <margleb@yandex.ru>
2020-04-25 18:12:37 +0300

    When Expressions - Including Exercises

--- app/src/main/java/com/example/myproject2/KotlinBasics.kt
+++ app/src/main/java/com/example/myproject2/KotlinBasics.kt
@@ -1,14 +1,42 @@
 package com.example.myproject2
 
 fun main() {
-  // if statement
-  var hightPerson1 = 170
-  var hightPerson2 = 189
-  if(hightPerson1 > hightPerson2) {
-      println("Use raw force")
-  } else if(hightPerson1 == hightPerson2) {
-      println("use your power technique 1337")
-  } else {
-      println("use technique")
-  }
+    var season = 3
+    // аналог switch
+    when(season) {
+        1 -> println("Spring")
+        2 -> println("Summer")
+        3 -> {
+            println("Fall")
+            println("Authumn")
+        }
+        4 -> println("Winter")
+        else -> println("Invalid Season") // default block
+    }
+    var month = 3
+    // c диапазонами
+    when(month){
+        in 3..5 -> println("Spring")
+        in 6..8 -> println("Summer")
+        in 9..11 -> println("Fall")
+        12, 1, 2 -> println("Winter")
+        // in 12 downTo 2 -> println("Winter")
+        else -> println("Invalid Season") // default block
+    }
+    val age = 15;
+    when(age) {
+        !in 0..20 -> println("now you may drink in ther US")
+        in 18..20 -> println("you may vote now")
+        16,17 -> println("you may drive now")
+        else -> println("you too young")
+    }
+    // any означает может быть любого типа
+    var x : Any = 13.37f
+    // is проверяет является ли типом
+    when(x) {
+        is Int -> println("$x is an Int")
+        is Double -> println("$x is a double")
+        is String -> println("$x is a String")
+        else -> println("$x is none of the above")
+    }
 }
