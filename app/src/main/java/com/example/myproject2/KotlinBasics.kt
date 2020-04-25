535ccf0d224bbe9726e0f3d0a33b9f6b9eebce59
margleb <margleb@yandex.ru>
2020-04-25 19:38:08 +0300

    Do While Loop

--- app/src/main/java/com/example/myproject2/KotlinBasics.kt
+++ app/src/main/java/com/example/myproject2/KotlinBasics.kt
@@ -1,10 +1,16 @@
 package com.example.myproject2
 
 fun main() {
-    var x = 99
+    var x = 1
     while(x >= 0) {
         print("$x ")
         x -= 2
     }
     println("\nwhile loop is done")
+    x = 1
+    do{
+        print("$x ")
+        x++
+    } while( x <= 10)
+    println("\ndo while loop is done")
 }
