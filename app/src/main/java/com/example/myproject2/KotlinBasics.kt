4cb5b24db98cb38a378edb9a345e3a094bd737e2
margleb <margleb@yandex.ru>
2020-04-25 19:50:23 +0300

    For Loops

--- app/src/main/java/com/example/myproject2/KotlinBasics.kt
+++ app/src/main/java/com/example/myproject2/KotlinBasics.kt
@@ -1,30 +1,16 @@
 package com.example.myproject2
 
 fun main() {
-    
-    var x = 1
-    while(x >= 0) {
-        print("$x ")
-        x -= 2
+    for(num in 1..10) {
+        print("$num ")
     }
-
-    println("\nwhile loop is done")
-    x = 1
-
-    do{
-        print("$x ")
-        x++
-    } while( x <= 10)
-    println("\ndo while loop is done")
-
-    var feltTemp = "cold"
-    var roomTemp = 10
-    while(feltTemp == "cold") {
-        roomTemp++
-        if(roomTemp != 20) {
-            feltTemp = "comfy"
-            println("it's comfy now")
-        }
+    println()
+    for(i in 1 until 10) {
+        print("$i ")
     }
-
+    println()
+    // same as - for(i in 10.downTo(1).step(2)
+    for(i in 10 downTo 1 step 2) {
+        print("$i ")
+    }
 }
