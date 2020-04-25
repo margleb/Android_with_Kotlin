1284da8f28c4f606da06f1bc291a2f8276ceefe1
margleb <margleb@yandex.ru>
2020-04-25 17:17:52 +0300

    Comparison Operators

--- app/src/main/java/com/example/myproject2/KotlinBasics.kt
+++ app/src/main/java/com/example/myproject2/KotlinBasics.kt
@@ -1,15 +1,13 @@
 package com.example.myproject2
 
 fun main() {
-    // Arithmetic operators (+,-,*,/,%)
-    var result = 10+8
-    // result /= 2 // разделить на 2
-    // result %= 2; // деление с остатком
-    val a = 5.0
-    val b = 3;
-    result = (a/b).toInt() // делаем результат int
-    var resultDouble : Double
-    resultDouble = a / b;
-    print(resultDouble)
+    // Arithmetic operators (==, !=, <, >, <=, >=)
+    val isEqual = 5==3;
+    println("isEqual is $isEqual")
+    val isNotEqual = 5!=5
+    // String interpolation
+    println("isNotEqual is $isNotEqual")
+    println("is5greater4 ${5>3}")
+    println("is5LowerEqual3 ${5<=3}")
 
 }
