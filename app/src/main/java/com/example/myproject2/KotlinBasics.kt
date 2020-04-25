bd27e57dd085dc431b74a53870c1ab3d497c9fbe
margleb <margleb@yandex.ru>
2020-04-25 15:04:31 +0300

    Datatypes of numbers

--- app/src/main/java/com/example/myproject2/KotlinBasics.kt
+++ app/src/main/java/com/example/myproject2/KotlinBasics.kt
@@ -1,10 +1,15 @@
 package com.example.myproject2
 
 fun main() {
-    // изменяемая переменная
-    var myName = "Gleb"
-    myName = "Franzis"
-    // Не изменяемая переменная
-    val mySurname = "Marianov";
-    print("Hello " + myName);
+    // TODO: Добавить новый функционал
+    var myName = "Frank" // тип String
+    var myAge = 31 // тип int
+    // integer types (принимают номера), можно указать точный тип данный, необходимый мне
+    val myByte: Byte = 13
+    val myShort: Short = 125
+    val myInt: Int = 123123
+    val myLong: Long = 12_039_812_309_131_123
+    // floating Point number Types: Float (32 bit), Double (64 bit);
+    val myFloat: Float = 13.37f;
+    val myDouble: Double = 3.32132132133
 }
