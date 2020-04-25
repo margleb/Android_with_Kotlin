ea44d72fd660bb05f3488a6fa7afa05cf59ee2cd
margleb <margleb@yandex.ru>
2020-04-25 16:49:37 +0300

    Datatypes bool, char and stringъ

--- app/src/main/java/com/example/myproject2/KotlinBasics.kt
+++ app/src/main/java/com/example/myproject2/KotlinBasics.kt
@@ -12,4 +12,16 @@ fun main() {
     // floating Point number Types: Float (32 bit), Double (64 bit);
     val myFloat: Float = 13.37f;
     val myDouble: Double = 3.32132132133
+    // Booleans
+    var isSunny: Boolean = true;
+    isSunny = false;
+    // Char
+    val letterChar = 'A'
+    val digitChar = '1'
+    // String
+    val myStr = "Hello World";
+    var firstCharInString = myStr[0];
+    var lastCharInStr = myStr[myStr.length - 1];
+    print("First character " + lastCharInStr);
+
 }
