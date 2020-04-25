b8b1b521951cb6ee18dd519c1ae01a39f93a0a5f
margleb <margleb@yandex.ru>
2020-04-25 19:42:48 +0300

    More On While Loops

--- app/src/main/java/com/example/myproject2/KotlinBasics.kt
+++ app/src/main/java/com/example/myproject2/KotlinBasics.kt
@@ -1,16 +1,30 @@
 package com.example.myproject2
 
 fun main() {
+    
     var x = 1
     while(x >= 0) {
         print("$x ")
         x -= 2
     }
+
     println("\nwhile loop is done")
     x = 1
+
     do{
         print("$x ")
         x++
     } while( x <= 10)
     println("\ndo while loop is done")
+
+    var feltTemp = "cold"
+    var roomTemp = 10
+    while(feltTemp == "cold") {
+        roomTemp++
+        if(roomTemp != 20) {
+            feltTemp = "comfy"
+            println("it's comfy now")
+        }
+    }
+
 }
