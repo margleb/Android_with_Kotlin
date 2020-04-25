33497418e8115f36a725e073e4229ee361a45c73
margleb <margleb@yandex.ru>
2020-04-25 17:39:32 +0300

    If Statements

--- app/src/main/java/com/example/myproject2/KotlinBasics.kt
+++ app/src/main/java/com/example/myproject2/KotlinBasics.kt
@@ -1,17 +1,14 @@
 package com.example.myproject2
 
 fun main() {
-    //Assign operators(+=,-=,*=,/=,%=0)
-    var myNum = 5
-    myNum += 3
-    println("myNum is $myNum")
-    myNum *= 4
-    println("myNum is $myNum")
-    // Increment/Decrement
-    myNum++
-    println("myNum is $myNum")
-    myNum--
-    println("myNum is $myNum")
-    println("myNum is ${myNum++}")
-    println("myNum is ${++myNum}")
+  // if statement
+  var hightPerson1 = 170
+  var hightPerson2 = 189
+  if(hightPerson1 > hightPerson2) {
+      println("Use raw force")
+  } else if(hightPerson1 == hightPerson2) {
+      println("use your power technique 1337")
+  } else {
+      println("use technique")
+  }
 }
