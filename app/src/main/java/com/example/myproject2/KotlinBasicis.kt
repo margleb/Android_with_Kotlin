45cd5a6098aa577f5017f841996f3b95cc51d460
margleb <margleb@yandex.ru>
2020-04-26 11:58:21 +0300

    Data Class

--- app/src/main/java/com/example/myproject2/KotlinBasicis.kt
+++ app/src/main/java/com/example/myproject2/KotlinBasicis.kt
@@ -1,34 +1,35 @@
 package com.example.myproject2
 
-import java.lang.IllegalArgumentException
+// класс для хранения данных
+// основной конструктор имеет как минимум один параметр
+// параметры конструктора должны быть как val или var
+// классы данных не могут быть абстрактными, open, sealed или inner
+data class User(val id: Long, var name: String)
 
 fun main() {
-    var myCar = Car()
-    println("brand is : ${myCar.myBrand}")
-    myCar.maxSpeed = 2
-    println("Maxspeed is ${myCar.maxSpeed}")
-    println("Model is ${myCar.myModel}");
-}
+    val user1 = User(1, "Denis")
+    //val name = user1.name
+    //println(name)
+    //user1.name = "Michael"
 
-class Car() {
-    // lateinit говорит о том, что данная переменная будет инициализирована позже
-    lateinit var owner : String
-    // пример гетера
-    val myBrand: String = "BMW"
-    get() {
-        return field.toLowerCase()
-    }
-    var maxSpeed: Int = 250
-    get() = field
-    set(value) {
-        field = if(value > 0) value else throw IllegalArgumentException("Max speed cannot be less than 0")
-    }
-    var myModel:String = "M5"
-    private set // устанавливаем приватным setter
-    
-    // инициализатор
-    init {
-        this.myModel = "M3"
-        this.owner = "Frank"
-    }
+    val user2 = User(2, "Michael")
+
+    // так как это класс данных, то тут уже есть по умолчанию методы:
+    // equels()/hashCode()
+    // println(user1.equals(user2))
+    // toString()
+    // println("User Details: $user1")
+    // сopy()
+    val updatedUser = user1.copy(name="Denis Panjuta")
+
+    // println(user1)
+    // println(updatedUser)
+
+    // компонентная функция, позволяющая распечатать значение компоненты
+    // println(updatedUser.component1()) // print 1
+    // println(updatedUser.component2()) // print Denis Panjuta
+
+    // сохранение в разные переменные значение полей
+    val(id, name) = updatedUser
+    println("id=$id, name=$name")
 }
