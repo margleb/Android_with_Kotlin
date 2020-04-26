367770df7574ad6fd92424da58826ae7b4fc3e21
margleb <margleb@yandex.ru>
2020-04-26 07:00:28 +0300

    Nullables - Elvis Operator - Not Null Assertion

--- app/src/main/java/com/example/myproject2/KotlinBasics.kt
+++ app/src/main/java/com/example/myproject2/KotlinBasics.kt
@@ -1,22 +1,16 @@
 package com.example.myproject2
 
 fun main() {
-   var name:String = "Denis"
-   // name = null
-   //  Non-Null тип
-   var nullableName:String? = "Denis"
-   // nullableName = null
+    var nullabeName:String? = "String"
+    nullabeName = null
 
-   // var len = name.length
-   // var len2 = nullableName?.length
-   // println(nullableName?.toLowerCase())
-   // Для проведения каких-либо операций исключительно над non-null значениями вы можете использовать let оператор вместе с оператором безопасного вызова
-   // it - указывает на этот элемент
-   nullableName?.let { println ( it.length )}
+    // подставляет по умолчанию если null Elvis operator
+    val name = nullabeName ?: "Guest"
+    println("name is $name")
+    // отменяет все защиты по notNull и если нулевой, то выдает ошибку NullPointerException
+    nullabeName!!.toLowerCase()
 
-   /* if(nullableName!=null) {
-       var len2 = nullableName.length
-   } else {
-       null
-   } */
+    // операторы можно использовать как цепочка
+    // val wifeAge : String? = user?.wife?.age ?: 0
+
 }
