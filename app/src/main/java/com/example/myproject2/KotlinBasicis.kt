d2ad6b3a78e3d2cf97b61fef4efb9e6ab5f7bc7a
margleb <margleb@yandex.ru>
2020-04-26 11:05:16 +0300

    Member Variables - Functions And Constructors

--- app/src/main/java/com/example/myproject2/KotlinBasicis.kt
+++ app/src/main/java/com/example/myproject2/KotlinBasicis.kt
@@ -1,14 +1,34 @@
 package com.example.myproject2
 
-var b = 3
+
 fun main() {
-    myFunction(5)
-    // b = 5
+    var denis = Person("Denis", "Panjuta", 31)
+    denis.hobby = "skateboard"
+    denis.age = 32
+    println("Denis is ${denis.age} years old")
+    denis.stateHobby()
+    var john = Person()
+    john.hobby = "play video games"
+    john.stateHobby()
 }
 
-// this a is a parementer
-fun myFunction(a:Int) {
-    // a is a variable
-    var b = a
-    println("b is $b")
+class Person(fitstName:String = "John", lastName:String = "Doe") {
+    // Member Variable - Properties
+    var age:Int? = null
+    var hobby :String = "watch Netflix"
+    var firstName:String? = null
+    init {
+        println("$fitstName and $lastName")
+    }
+    // Member  secondary Constructor
+    constructor(firstName:String, lastName:String, age: Int) : this(firstName, lastName) {
+        this.age = age
+        println("$firstName and $lastName age is $age")
+    }
+    // Member functions - Methods
+    fun stateHobby() {
+        this.firstName = "Gleb"
+        println("$firstName\'s hobby is $hobby")
+    }
+
 }
