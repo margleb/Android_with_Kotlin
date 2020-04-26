14ec493aa7bbce8f42fe93b011766228a0c62057
margleb <margleb@yandex.ru>
2020-04-26 17:35:59 +0300

    Interfaces

--- app/src/main/java/com/example/myproject2/KotlinBasicis.kt
+++ app/src/main/java/com/example/myproject2/KotlinBasicis.kt
@@ -1,35 +1,61 @@
 package com.example.myproject2
 
-// класс для хранения данных
-// основной конструктор имеет как минимум один параметр
-// параметры конструктора должны быть как val или var
-// классы данных не могут быть абстрактными, open, sealed или inner
-data class User(val id: Long, var name: String)
 
 fun main() {
-    val user1 = User(1, "Denis")
-    //val name = user1.name
-    //println(name)
-    //user1.name = "Michael"
+    var audiA3 = Car(200.0, "A3", "Audi")
+    var teslaS = ElectricCar(240.0,"S-model", "Tesla", 85.0)
+    teslaS.chargerType = "Type2"
+    // teslaS.extendRange(200.0)
 
-    val user2 = User(2, "Michael")
+    // teslaS.drive()
+    teslaS.breake()
+    audiA3.breake()
 
-    // так как это класс данных, то тут уже есть по умолчанию методы:
-    // equels()/hashCode()
-    // println(user1.equals(user2))
-    // toString()
-    // println("User Details: $user1")
-    // сopy()
-    val updatedUser = user1.copy(name="Denis Panjuta")
+    // Polymorphism
+    // audiA3.drive(200.0)
+    // teslaS.drive(200.0)
+}
 
-    // println(user1)
-    // println(updatedUser)
 
-    // компонентная функция, позволяющая распечатать значение компоненты
-    // println(updatedUser.component1()) // print 1
-    // println(updatedUser.component2()) // print Denis Panjuta
+interface Drivable {
+    val maxSpeed: Double
+    fun drive():String
+    fun drive(distance:Double):String
+    fun breake() { // можно указать по умолчанию
+        println("The drivable is breaking")
+    }
+}
 
-    // сохранение в разные переменные значение полей
-    val(id, name) = updatedUser
-    println("id=$id, name=$name")
+// расширяем интерфейсом Drivable, а также добавляем поле, необходимое для перезаписи
+open class Car(override val maxSpeed: Double, val name:String, val brand:String) : Drivable {
+    open var range: Double = 0.0
+    // перезаписанный метод
+    override fun drive(distance: Double): String = "Drove for $distance KM on regularCar"
+
+    fun extendRange(amount: Double) {
+        if(amount > 0) {
+            range += amount
+        }
+    }
+
+    // перезаписанный метод
+    override fun drive(): String = "driving the interface drive"
+}
+
+// так как в интерфейс был включен в родительский класс, то здесь тоже нужно расширить
+class ElectricCar(maxSpeed: Double, name: String, brand:String, batteryLife: Double) : Car(maxSpeed,name, brand) {
+    var chargerType = "Type1"
+    override var range = batteryLife * 6
+
+    // перезаписанный метод
+    override fun drive(distance: Double): String = "Drove for $distance KM on electricity"
+
+    // допускается такой же вариант перезаписи
+    override fun drive(): String {
+        return "Drove for $range КM on electircity"
+    }
+    override fun breake() {
+        super.breake()
+        println("breake inside of electric car")
+    }
 }
