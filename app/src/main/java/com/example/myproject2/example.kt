f6f92840efbf4ecb34c91c0ac262e2aa43f0c5f5
margleb <margleb@yandex.ru>
2020-04-27 11:07:37 +0300

    Exception Handling With Try And Catch

--- app/src/main/java/com/example/myproject2/example.kt
+++ app/src/main/java/com/example/myproject2/example.kt
@@ -1,34 +1,11 @@
 package com.example.myproject2
 
-class Fruit {
-    private var watermelon: String = "watermelon"
-    private var grapes: String = "grapes"
-    private var greenApple: String = "greenApple"
-    // Вложенный класс это класс который создан внутри другого класса
-    // В Котлине вложенный класс по умолчанию статический, поэтому доступ к членам класса может быть получить без сощдания обьекта данного класса
-    // Вложенные классы не могут иметь доступ до переменых членов внешнего класса
-    class Nested {
-        fun sayHello() {
-            // нельзя получить доступ до переменных
-            // println("$watermelon + $grapes + $greenApple")
-        }
-    }
-    // Внутренний класс - это класс который создает внутри другого класса с ключевым словом inner
-    // Внутренний класс не может быть облевлен внутри интерфейся или не внутрненнего вложенного класса
-    // Приемущество внутреннего класса перед вложенным классом то, что они может иметь доступ до члено внешнего класса, даже если они обьявлены приватными
-    // Внутренник классы содержат ссылку на обьект их внешнего класса
-    inner class Inner {
-        fun getAppleEmoji():String {
-            println("I am a function inside a INNER class")
-            println("apple emoji is $greenApple")
-            return greenApple
-        }
-    }
-}
-
-fun main(args: Array<String>) {
-    Fruit.Nested().sayHello()
-    // необходимо инициализировать перед тем, как получать данные до внутреннего класса
-    var fruit = Fruit()
-    fruit.Inner().getAppleEmoji();
+fun main() {
+    // Иногда невозможно скастить переменну и она выбрасывает исключение, это называется "небезопасный кастинг"
+    var epic: Any? = null;
+    // безопасный кастинг
+    var str:String? = epic as? String
+    // небезопасный кастинг
+    // var str:String = epic as String
+    println(str)
 }
