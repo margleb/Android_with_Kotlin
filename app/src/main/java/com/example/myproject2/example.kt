88d963305ae5d03d2b80cebdad461ff1deb2ca32
margleb <margleb@yandex.ru>
2020-04-27 10:56:48 +0300

    Nested and Inner Classes

--- app/src/main/java/com/example/myproject2/example.kt
+++ app/src/main/java/com/example/myproject2/example.kt
@@ -1,30 +1,34 @@
 package com.example.myproject2
-// В kotlin предусмотрено 4 модификтаора доступа private/protected/internal/public
 
-fun main() {
-    // Если явно не используется никакого модификатора, то он по умолчанию применяется как public
-}
-
-// Если явно не используется никакого модификатора, то он по умолчанию применяется как public
-class PublicVisible(name:String) {
-    init {
-        println("Hello $name this class has public access")
+class Fruit {
+    private var watermelon: String = "watermelon"
+    private var grapes: String = "grapes"
+    private var greenApple: String = "greenApple"
+    // Вложенный класс это класс который создан внутри другого класса
+    // В Котлине вложенный класс по умолчанию статический, поэтому доступ к членам класса может быть получить без сощдания обьекта данного класса
+    // Вложенные классы не могут иметь доступ до переменых членов внешнего класса
+    class Nested {
+        fun sayHello() {
+            // нельзя получить доступ до переменных
+            // println("$watermelon + $grapes + $greenApple")
+        }
+    }
+    // Внутренний класс - это класс который создает внутри другого класса с ключевым словом inner
+    // Внутренний класс не может быть облевлен внутри интерфейся или не внутрненнего вложенного класса
+    // Приемущество внутреннего класса перед вложенным классом то, что они может иметь доступ до члено внешнего класса, даже если они обьявлены приватными
+    // Внутренник классы содержат ссылку на обьект их внешнего класса
+    inner class Inner {
+        fun getAppleEmoji():String {
+            println("I am a function inside a INNER class")
+            println("apple emoji is $greenApple")
+            return greenApple
+        }
     }
 }
-// Видин только внутри класса example
-private class PrivateVisible {
-    fun isPrivate() {
-        println("You can't have a access becouse it private")
-    }
+
+fun main(args: Array<String>) {
+    Fruit.Nested().sayHello()
+    // необходимо инициализировать перед тем, как получать данные до внутреннего класса
+    var fruit = Fruit()
+    fruit.Inner().getAppleEmoji();
 }
-
-// модификатор protected не приминем внутри файла (топлевел уровень)
-// protected class ProtectedVisible {}
-
-// Доступно только в модуле, каком то наборе файлов maven/groovy и так далее
-internal class InternalVisible {
-    init {
-        println("This class has a internal access, that's mean have access only at same module")
-    }
-}
-
--- app/src/main/java/com/example/myproject2/mySecond.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myproject2
-
-fun main() {
-    PublicVisible("Gleb")
-    // PrivateVisible() // private - cannot have a acess
-    InternalVisible()
-}
