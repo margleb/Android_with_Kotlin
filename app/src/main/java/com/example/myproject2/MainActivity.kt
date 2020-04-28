43fd8704cf24c8836f2191912c78622e3dac6794
margleb <margleb@yandex.ru>
2020-04-28 10:22:08 +0300

    Adding The onDecimalPoint Function

--- app/src/main/java/com/example/myproject2/MainActivity.kt
+++ app/src/main/java/com/example/myproject2/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.myproject2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var lastNumeric:Boolean = false
+    var lastDot:Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,10 +18,23 @@ class MainActivity : AppCompatActivity() {
     // используется атрибут в XML onClick
     fun onDigit(view: View) {
         tvInput.append((view as Button).text) // добавляем текст с кнопки
+        lastNumeric = true
     }
 
     fun onClear(view: View) {
         tvInput.text = ""
+        lastNumeric = false
+        lastDot = false
+    }
+
+    fun onDecimalPoint(view:View) {
+        // если значение до было номером
+        // и не было точкой до этого
+        if(lastNumeric && !lastDot) {
+            tvInput.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
     }
 
 }
