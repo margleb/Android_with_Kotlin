f1938323ca6c8adde35965f5fb919f39ba26a522
margleb <margleb@yandex.ru>
2020-04-28 13:59:18 +0300

    Setting Up The UI Using Cardview And Disable The Statusbar

--- app/src/main/java/com/example/myproject2/MainActivity.kt
+++ app/src/main/java/com/example/myproject2/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.myproject2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // удаляет верхнюю панель с экрана
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
     }
 }
