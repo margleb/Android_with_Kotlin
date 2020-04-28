d1f409868a71cb2dd916e3a7803408a51463fc28
margleb <margleb@yandex.ru>
2020-04-28 14:34:11 +0300

    Creating The Question Model And Preparing The Questions

--- app/src/main/java/com/example/myproject2/MainActivity.kt
+++ app/src/main/java/com/example/myproject2/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.myproject2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +14,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         // удаляет верхнюю панель с экрана
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        btn_start.setOnClickListener {
+            if(et_name.text.toString().isEmpty()) {
+                Toast.makeText(this, "The field cannot be empty", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+
     }
 }
