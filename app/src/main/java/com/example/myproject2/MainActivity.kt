43ac1f2d2271cfd42c02be9470b662bf9ca0824b
margleb <margleb@yandex.ru>
2020-04-28 11:54:06 +0300

    Adding The Missing Operations And Finalizing The App

--- app/src/main/java/com/example/myproject2/MainActivity.kt
+++ app/src/main/java/com/example/myproject2/MainActivity.kt
@@ -73,12 +73,49 @@ class MainActivity : AppCompatActivity() {
                     if(!prefix.isEmpty()) {
                         one = prefix + one
                     }
-                    tvInput.text = (one.toDouble() - two.toDouble()).toString()
+                    tvInput.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
+                } else if(tvValue.contains("+")) {
+                    val splitValue = tvValue.split("+")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    // если значение изначально минусовое было, то возращаем минус обратно
+                    if(!prefix.isEmpty()) {
+                        one = prefix + one
+                    }
+                    tvInput.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+                }  else if(tvValue.contains("*")) {
+                    val splitValue = tvValue.split("*")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    // если значение изначально минусовое было, то возращаем минус обратно
+                    if(!prefix.isEmpty()) {
+                        one = prefix + one
+                    }
+                    tvInput.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
+                }  else if(tvValue.contains("/")) {
+                    val splitValue = tvValue.split("/")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    // если значение изначально минусовое было, то возращаем минус обратно
+                    if(!prefix.isEmpty()) {
+                        one = prefix + one
+                    }
+                    tvInput.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
                 }
+
             } catch (e: ArithmeticException) {
                 e.printStackTrace()
             }
         }
+    }
+
+    private fun removeZeroAfterDot(result:String):String {
+        var value = result
+        if(result.contains(".0")) {
+            // удалем .0 из числа
+            return result.substring(0, result.length - 2)
+        }
+        return value
     }
 
 }
