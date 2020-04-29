220247da997d6e35e24376c26b6f462fc5d5b060
margleb <margleb@yandex.ru>
2020-04-29 19:32:31 +0300

    Selecting The Brush Size From A Custom Dialog That We Created

--- app/src/main/java/com/example/myproject2/MainActivity.kt
+++ app/src/main/java/com/example/myproject2/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.myproject2
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_brush_size.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +13,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         // устанавливаем толщину кисти в соответствии с размером экрана
         drawing_view.setSizeForBrush(20.toFloat())
+        ib_brush.setOnClickListener{
+            showBrushSizeChooserDialog()
+        }
+    }
+
+    private fun showBrushSizeChooserDialog() {
+        // создает диалоговое окно по умолчанию
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size: ")
+        val smallBtn = brushDialog.ib_small_brush
+        val mediumBtn = brushDialog.ib_medium_brush
+        val largeBtn = brushDialog.ib_large_brush
+        // устанавливаем размер кисти на 10
+        smallBtn.setOnClickListener{
+            drawing_view.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+        mediumBtn.setOnClickListener{
+            drawing_view.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+        largeBtn.setOnClickListener{
+            drawing_view.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
+        brushDialog.show()
     }
 }
