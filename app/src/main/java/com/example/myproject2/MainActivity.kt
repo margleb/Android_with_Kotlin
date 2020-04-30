25a34c0cf3a6cf36ed3bfa1b7f5b62846384f86a
margleb <margleb@yandex.ru>
2020-04-30 14:47:16 +0300

    Permissions Demo

--- app/src/main/java/com/example/myproject2/MainActivity.kt
+++ app/src/main/java/com/example/myproject2/MainActivity.kt
@@ -1,12 +1,55 @@
 package com.example.myproject2
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    // класс, содержащий константы
+    companion object {
+        private const val CAMERA_PERMISSION_CODE = 1
+        private const val FINE_LOCATION_PERMISSION_CODE = 2
+        private const val CAMERA_AND_FINE_LOCATION_PERMISSION_CODE = 12
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnCameraPermission.setOnClickListener{
+            if (
+                ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+            )
+            {
+                Toast.makeText(this, "You already have access for camera and location", Toast.LENGTH_SHORT).show()
+            }
+            else
+            {
+                // Request Permission
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA, Manifest.permission.ACCESS_FINE_LOCATION), CAMERA_AND_FINE_LOCATION_PERMISSION_CODE)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == CAMERA_AND_FINE_LOCATION_PERMISSION_CODE) {
+            if(grantResults.isNotEmpty() && (grantResults[0] == PackageManager.PERMISSION_DENIED || grantResults[1] == PackageManager.PERMISSION_DENIED)) {
+                Toast.makeText(this, "You need permission for camera and location!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "You already has a permission for location and camera", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
