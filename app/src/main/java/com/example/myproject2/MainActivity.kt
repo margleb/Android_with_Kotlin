65d37035cd04ec77a23fe6409f6abd94bb5d0e92
margleb <margleb@yandex.ru>
2020-04-30 11:26:51 +0300

    Adding The Color Pallet Using Custom Drawables That We Created

--- app/src/main/java/com/example/myproject2/MainActivity.kt
+++ app/src/main/java/com/example/myproject2/MainActivity.kt
@@ -3,16 +3,26 @@ package com.example.myproject2
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_brush_size.*
 
 class MainActivity : AppCompatActivity() {
+    private var mImageButtonCurrentPaint: ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // устанавливаем толщину кисти в соответствии с размером экрана
         drawing_view.setSizeForBrush(20.toFloat())
+
+        // черный цвет (получаем позицию данного элемента)
+        mImageButtonCurrentPaint = ll_paint_colors[1] as ImageButton
+        // устанавливаем нажатое сосотояние кнопки
+        mImageButtonCurrentPaint!!.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.pallet_pressed))
+
         ib_brush.setOnClickListener{
             showBrushSizeChooserDialog()
         }
